ce7c499db28c93bccaad2ab48b21ac6bdfa5a98b
Jesse Kipp <jesse.kipp@huffingtonpost.com>
2016-10-06 11:13:45 -0400

    Change Probe constant name to avoid collisions

--- spec/unit/probes/httpclient_spec.rb
+++ spec/unit/probes/httpclient_spec.rb
@@ -5,15 +5,15 @@ module Skylight
     describe "HTTPClient:Probe", :httpclient_probe, :probes do
 
       it "is registered" do
-        reg = Skylight::Probes.installed["::HTTPClient"]
-        expect(reg.klass_name).to eq("::HTTPClient")
+        reg = Skylight::Probes.installed["HTTPClient"]
+        expect(reg.klass_name).to eq("HTTPClient")
         expect(reg.require_paths).to eq(["httpclient"])
-        expect(reg.probe).to be_a(Skylight::Probes::HTTPClient::Probe)
+        expect(reg.probe).to be_a(Skylight::Probes::HTTPClientProbe::Probe)
       end
 
       it "wraps HTTPClient#do_request" do
         # This test is somewhat lame
-        expect(::HTTPClient.private_instance_methods).to include(:do_request_without_sk)
+        expect(HTTPClient.private_instance_methods).to include(:do_request_without_sk)
       end
 
     end
